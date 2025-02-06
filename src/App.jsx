26d776434fcd1fd6cf91cba532caf0e1c9f2d79c
apricopt <apricopt@gmail.com>
2025-02-07 00:53:22 +0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import MobileNavbar from './Components/MobileNavbar'
 import 'aos/dist/aos.css'
 import AOS from 'aos'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const App = () => {
 
@@ -54,15 +56,23 @@ const App = () => {
         <section id='tiles' className="text-gray-100 bg-[#1F1F1F] body-font">
         <div className="container px-5 md:py-20 py-10 mx-auto flex flex-wrap">
           <div className="lg:w-[90%] w-full mx-auto">
-          <div className="flex flex-wrap mb-4 -mx-2 md:space-y-0 space-y-6">
+          <div className="flex flex-wrap mb-4 -mx-2 md:space-y-0 space-y-2">
               <div className="px-2 md:w-1/2 w-full md:h-[320px] h-[180px] group" data-aos="fade-right"
      data-aos-offset="300"
      data-aos-easing="ease-in-sine"
      data-aos-duration="1000">
                 <div className="flex flex-wrap justify-center w-full h-full items-center py-4 px-4 relative">
-                  <img loading="lazy" alt="gallery" className=" object-contain w-full h-full group-hover:border object-center block absolute inset-0 rounded-lg group-hover:opacity-10 transition-all" src="/tile-1.png" />
+                  <LazyLoadImage
+                    effect="blur" 
+                    alt="gallery" 
+                    height={'100%'} 
+                    width={'100%'} 
+                    className="object-contain object-center block absolute inset-0 rounded-lg transition-all"
+                    wrapperClassName="w-full h-full group-hover:border group-hover:opacity-10" 
+                    src="/tile-1.png" 
+                  />
                   <div className="text-center px-4 absolute z-10 w-full">
-                  <div className='justify-start items-center md:h-[300px] h-full relative text-left group-hover:flex hidden gap-3'>
+                  <div className='justify-start items-center md:h-[300px] h-full relative text-left group-hover:flex hidden gap-3 px-2'>
 
                 <div>
                   <h1 className=' text-xl font-extrabold border-b-2 border-blue-400 inline'>SERVER INFO</h1>
@@ -81,11 +91,12 @@ const App = () => {
 
                 </div>
 
-      <div className=' absolute bottom-0 md:right-0 -right-3 flex flex-col justify-end items-end'>
+      <div className=' absolute bottom-0 md:right-0 -right-0 flex flex-col justify-end items-end'>
       <div className=' relative'>
 
         <a href='https://shop.glitchedrealms.com/' className="mt-3 text-indigo-500 inline-flex" >
-          <img loading="lazy" src='/tiles_btn.png' className=' md:w-40 w-36' />
+          <LazyLoadImage effect="blur"  height="auto"
+      width="100%" src='/tiles_btn.png' className=' md:w-40 w-36' />
         </a>
       </div>
         <p className=' text-sm -mt-1 hidden md:block'>Please join manually from console/server list if the link is not working</p>
@@ -100,9 +111,11 @@ const App = () => {
      data-aos-easing="ease-in-sine"
      data-aos-duration="2000">
                 <div className="flex flex-wrap group justify-center w-full h-full items-center py-4 px-4 relative">
-                  <img loading="lazy" alt="gallery" className="object-contain w-full h-full group-hover:border object-center block absolute inset-0 rounded-lg group-hover:opacity-10 transition-all" src="/tile-2.png" />
+                  <LazyLoadImage effect="blur"  height="100%"
+      width="100%" alt="gallery" className="object-contain object-center block absolute inset-0 rounded-lg transition-all"
+      wrapperClassName="w-full h-full group-hover:border group-hover:opacity-10" src="/tile-2.png" />
                   <div className="text-center px-4 absolute z-10 w-full">
-                  <div className='justify-start items-center md:h-[300px] h-full relative text-left group-hover:flex hidden gap-3'>
+                  <div className='justify-start items-center md:h-[300px] h-full relative text-left group-hover:flex hidden gap-3 px-2'>
 
       <div>
         <h1 className=' text-xl font-extrabold border-b-2 border-blue-400 inline'>SERVER INFO</h1>
@@ -120,9 +133,10 @@ const App = () => {
 
       </div>
 
-      <div className=' absolute bottom-0 md:right-0 -right-3 flex flex-col justify-end items-end'>
+      <div className=' absolute bottom-0 md:right-0 -right-0 flex flex-col justify-end items-end'>
         <a href='https://shop.glitchedrealms.com/' target='_blank' className="mt-3 text-indigo-500 inline-flex" >
-          <img loading="lazy" src='/tiles_btn.png' className=' md:w-40 w-36' />
+          <LazyLoadImage effect="blur"  height="auto"
+      width="100%" src='/tiles_btn.png' className=' md:w-40 w-36' />
         </a>
           <p className=' text-sm hidden md:block'>Please join manually from console/server list if the link is not working</p>
       </div>
@@ -131,16 +145,26 @@ const App = () => {
                 </div>
               </div>
             </div>
-            <div className="flex md:h-[310px] hover:md:h-[310px] h-[180px] hover:h-[200px] flex-wrap w-full hover:md:py-0 md:py-[93px] relative mb-4 group justify-center items-center" data-aos="fade-up"
+            <div className="flex md:h-[310px] hover:md:h-[310px] h-[180px] hover:h-[200px] flex-wrap w-full relative mb-4 group transition-all justify-center items-center px-3 py-2" data-aos="fade-up"
      data-aos-easing="ease-in"
      data-aos-offset="300"
      data-aos-duration="1000">
-              <img loading="lazy" alt="gallery" className=" md:block hidden object-contain w-full h-full group-hover:border object-center absolute inset-0 rounded-lg group-hover:opacity-10 transition-all" src="/tile-3.png" />
-              <img loading="lazy" alt="gallery" className=" md:hidden block object-contain w-full h-full group-hover:border object-center absolute inset-0 rounded-lg group-hover:opacity-10 transition-all" src="/tiles-mobile-3.png" />
+     <div className='relative h-full w-full md:block hidden'>
+
+              <LazyLoadImage effect="blur" height="100%"
+      width="100%" alt="gallery" className="md:block !w-full !h-full object-center absolute inset-0 rounded-lg !transition-all " 
+      wrapperClassName="hidden w-full h-full group-hover:border group-hover:opacity-10 delay-200" src="/tile-3.png" />
+     </div>
+     <div className='relative h-full w-full md:hidden block'>
+
+              <LazyLoadImage effect="blur"  height="100%"
+      width="100%" alt="gallery" className="md:hidden !w-full !h-full object-center absolute inset-0 rounded-lg !transition-all " 
+      wrapperClassName="block w-full h-full group-hover:border group-hover:opacity-10 delay-200" src="/tiles-mobile-3.png" />
+      </div>
               <div className="md:text-center px-4 absolute z-10 w-full">
                     {/* <h2 className="text-xl text-gray-900 font-medium title-font mb-2">Shooting Stars</h2>
                     <p className="leading-relaxed">Skateboard +1 mustache fixie paleo lumbersexual.</p> */}
-                    <div className='md:justify-start md:items-center h-full relative text-left group-hover:flex hidden gap-3'>
+                    <div className='md:justify-start md:items-center h-full relative text-left group-hover:flex hidden gap-3 px-2'>
 
                       <div>
                         <h1 className=' text-xl font-extrabold border-b-2 border-blue-400 inline'>SERVER INFO</h1>
@@ -157,7 +181,7 @@ const App = () => {
 
                       </div>
 
-                      <div className=' absolute -md:bottom-10 -bottom-2 md:right-0 -right-2 flex flex-col justify-end items-end'>
+                      <div className=' absolute -md:bottom-10 -bottom-2 md:right-0 -right-0 flex flex-col justify-end items-end'>
 
                       <div className=' relative'>
 
@@ -167,12 +191,14 @@ const App = () => {
               </div>
             )}
         <button onClick={handleCopy} className="mt-3 text-indigo-500 inline-flex" >
-           <img loading="lazy" src='/plat_now_button.png' className=' w-40' />
+           <LazyLoadImage effect="blur"  height="auto"
+      width="100%" src='/plat_now_button.png' className=' w-40' />
         </button>
       </div>
                       
                         {/* <button onClick={handleCopyRust} target='_blank' className="mt-3 text-indigo-500 inline-flex" >
-                          <img loading="lazy" src='/plat_now_button.png' className=' w-40' />
+                          <LazyLoadImage effect="blur"  height="auto"
+      width="100%" src='/plat_now_button.png' className=' w-40' />
                         </button>
                         <p className=' text-sm'>Please join manually from console/server list if the link is not working</p> */}
 
@@ -201,20 +227,23 @@ const App = () => {
             <div className=" md:w-1/2 hidden md:block" data-aos="fade-left"
      data-aos-easing="ease-in"
      data-aos-duration="2000">
-              <img loading="lazy" className="object-cover object-center clippy h-[500px]" alt="hero" src="/banner-1.png" />
+              <LazyLoadImage effect="blur"  height="auto"
+      width="100%" className="object-cover object-center clippy h-[500px]" alt="hero" src="/banner-1.png" />
             </div>
           </div>
         </section>
 
         <div className=' fixed transition-all hover:mr-0 cursor-pointer -mr-[120px] top-80 right-0 w-48 z-[1111]'>
               <a href='https://discord.gg/nmmr3CHRZJ' target='_blank'>
-                <img loading="lazy" src="/discord.png" alt="annie" className=""/>
+                <LazyLoadImage effect="blur"  height="auto"
+      width="100%" src="/discord.png" alt="annie" className=""/>
               </a>
         </div>
 
         <div className=' fixed transition-all animated hover:animate-none hover:mt-0 cursor-pointer z-[1111] -mt-[45px] top-0 md:right-4 right-10 w-48'>
               <a href='https://glitchbox.net' target='_blank'>
-                <img loading="lazy" src="/G_BUTTON.png" alt="annie" className=""/>
+                <LazyLoadImage effect="blur"  height="auto"
+      width="100%" src="/G_BUTTON.png" alt="annie" className=""/>
               </a>
         </div>
 
