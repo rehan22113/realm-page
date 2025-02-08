e96257e9eba32fce71443ae4936b688947821008
apricopt <apricopt@gmail.com>
2025-02-08 19:54:08 +0500

    updated

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,6 +4,7 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa6";
 import { FaFacebook } from "react-icons/fa";
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 const Footer = () => {
   return (
@@ -11,7 +12,9 @@ const Footer = () => {
    <footer className="text-white bg-[#141414] body-font">
   <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
   <a href="/" className="flex title-font font-bold items-center text-white text-xl mr-2 mb-4 md:mb-0">
-      <img src="/footer-logo.png" alt="logo" className="w-32" />
+   <LazyLoadImage
+               effect="blur"  height="auto"
+        width="100%" src='/footer-logo.webp' className='w-32' />
     </a>
     <p className="text-sm text-center md:text-left text-white sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© {new Date().getFullYear()} Glitched Realms —
       <a href="https://glitchbox.net" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">Powered by GLITCHBOX</a>
